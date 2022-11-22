3b0edcebcb12da621091285575d35768f6076ab4
Jigesh Sheoran <106379604+sheoraninfosec@users.noreply.github.com>
2022-11-23 04:50:47 +0530

    edit order-list-processing

---
+++ order-list-processing.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function processOrderList(orderList, orderId, state) {
+    // Write code here
+    return state === 'Processing' ?
+      orderList.map(item => ({
+        ...item,
+        state: item.id === orderId ? 'Processing' : item.state
+      })) :
+      orderList.filter(item => item.id !== orderId);
+}
+
+...
